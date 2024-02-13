80c0bb15cf680ad46ad41670f40918ab3ce95466
Abhay J Sharma <133881460+hi-abhay2004@users.noreply.github.com>
2024-02-13 22:31:58 +0530

    files being uploded

---
+++ index.js
@@ -0,0 +1,34 @@
+var crsr = document.querySelector("#cursor")
+var crsr_blur = document.querySelector("#cursor-blur")
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left=dets.x+"px"
+    crsr.style.top=dets.y+"px"
+    crsr_blur.style.left=dets.x-200+"px"
+    crsr_blur.style.top=dets.y-200 +"px"
+})
+
+
+gsap.to("#nav",{
+    backgroundColor:"#000",
+    height: "110px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1
+    }
+})
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        markers:true,
+        start:"top -25%",
+        end:"top -70vh",
+        scrub:3
+    }
+})
