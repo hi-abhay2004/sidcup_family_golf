16608967eddd42437f6c765eda6dff31411b113c
Abhay J Sharma <abhayjsharma440@gmail.com>
2024-02-15 23:23:28 +0530

    all files uploded
    all code and files have been uploded

--- index.js
+++ index.js
@@ -1,34 +1,133 @@
 var crsr = document.querySelector("#cursor")
 var crsr_blur = document.querySelector("#cursor-blur")
-document.addEventListener("mousemove",function(dets){
-    crsr.style.left=dets.x+"px"
-    crsr.style.top=dets.y+"px"
-    crsr_blur.style.left=dets.x-200+"px"
-    crsr_blur.style.top=dets.y-200 +"px"
+document.addEventListener("mousemove", function (dets) {
+    crsr.style.left = dets.x+30 + "px"
+    crsr.style.top = dets.y+ "px"
+    crsr_blur.style.left = dets.x - 200 + "px"
+    crsr_blur.style.top = dets.y - 200 + "px"
+    
+})
+
+var h4every = document.querySelectorAll("#nav h4");
+var h1every = document.querySelectorAll("#page4");
+var h1every = document.querySelectorAll("#page4");
+h4every.forEach(function(elem){
+    console.log(elem);
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale=4
+        crsr.style.border="0.1px solid white"
+        crsr.style.backgroundColor="transparent"
+    })
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale=1
+        crsr.style.border="0px solid white"
+        crsr.style.backgroundColor="#95C11E"
+
+    })
+})
+h1every.forEach(function(elem){
+    console.log(elem);
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale=4
+        crsr.style.border="0.1px solid white"
+        crsr.style.backgroundColor="transparent"
+        
+    })
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale=1
+        crsr.style.border="0px solid white"
+        crsr.style.backgroundColor="#95C11E"
+    })
 })
 
 
-gsap.to("#nav",{
-    backgroundColor:"#000",
+gsap.to("#nav", {
+    backgroundColor: "#000",
     height: "110px",
-    duration:0.5,
+    duration: 0.5,
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        markers: true,
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 1
+    }
+})
+gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
+        markers: true,
+        start: "top -25%",
+        end: "top -70vh",
+        scrub: 3
+    }
+})
+
+gsap.from("#about-us img,#about-us-in",{
+    y:90,
+    opacity:0,
+    duration:1,
     scrollTrigger:{
-        trigger:"#nav",
+        trigger:"#about-us",
         scroller:"body",
-        markers:true,
-        start:"top -10%",
-        end:"top -11%",
+        // markers:true,
+        start:"top 70%",
+        end:"top 65%",
         scrub:1
     }
 })
-gsap.to("#main",{
-    backgroundColor:"#000",
+
+gsap.from(".card",{
+    scale:0.8,
+    opacity:0,
+    duration:1,
+    stagger:0.1,
     scrollTrigger:{
-        trigger:"#main",
+        trigger:".card",
         scroller:"body",
+        start:"top 70%",
+        end:"top 65%",
         markers:true,
-        start:"top -25%",
-        end:"top -70vh",
+        scrub:2
+    }
+})
+
+
+gsap.from("#quote1",{
+  y:-70,
+  x:-70,
+  scrollTrigger:{
+    trigger:"#quote1",
+    scroller:"body",
+    start:"top 50%",
+    end:"top 45%",
+    scrub:4
+  }
+})
+gsap.from("#quote2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+      trigger:"#quote1",
+      scroller:"body",
+    //   markers:true,
+      start:"top 50%",
+      end:"top 45%",
+      scrub:4
+    }
+  })
+gsap.from("#page4 h1",{
+    y:50,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller:"body",
+        // markers:true,
+        start:"top 85%",
+        end:"top 80%",
         scrub:3
     }
+
 })
